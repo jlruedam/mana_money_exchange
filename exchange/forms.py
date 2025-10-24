47d73496735decc8cb3dfd0a4a1c90a7e634a75f
jlruedam <jlruedam@gmail.com>
2025-10-24 15:14:49 -0500

    Add flag selection to Divisa form, enhance modal functionality, and update styles

--- exchange/forms.py
+++ exchange/forms.py
@@ -6,7 +6,16 @@ class DivisaForm(forms.ModelForm):
         model = Divisa
         fields = ['nombre', 'compra', 'venta', 'bandera']
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, flag_choices=None, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        if flag_choices is not None:
+            self.fields['bandera'] = forms.ChoiceField(
+                choices=flag_choices,
+                widget=forms.Select(attrs={'class': 'form-control'})
+            )
+
+        # Apply 'form-control' class to other fields as well
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'bandera': # 'bandera' already has the class
+                field.widget.attrs['class'] = 'form-control'
--- exchange/views.py
+++ exchange/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 import json
+import requests
 
 from .models import Divisa
 from .forms import DivisaForm
+
+def get_flag_choices():
+    try:
+        response = requests.get("https://flagcdn.com/en/codes.json")
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        flag_data = response.json()
+        # Sort by country name for better UX
+        choices = sorted([(code, name) for code, name in flag_data.items()], key=lambda x: x[1])
+        choices.insert(0, ('', '-- Seleccione una bandera --'))
+        return choices
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching flag data: {e}")
+        return [('', '-- Error al cargar banderas --')]
 
 def index(request):
     divisas = Divisa.objects.all()
@@ -30,20 +44,23 @@ def tv_view(request):
 
 def divisa_list(request):
     divisas = Divisa.objects.all()
-    form = DivisaForm()
+    flag_choices = get_flag_choices()
+    form = DivisaForm(flag_choices=flag_choices)
     return render(request, 'exchange/divisa_list.html', {'divisas': divisas, 'form': form})
 
 def divisa_create(request):
+    flag_choices = get_flag_choices()
     if request.method == 'POST':
-        form = DivisaForm(request.POST)
+        form = DivisaForm(request.POST, flag_choices=flag_choices)
         if form.is_valid():
             form.save()
     return redirect('divisa_list')
 
 def divisa_edit(request, pk):
     divisa = get_object_or_404(Divisa, pk=pk)
+    flag_choices = get_flag_choices()
     if request.method == 'POST':
-        form = DivisaForm(request.POST, instance=divisa)
+        form = DivisaForm(request.POST, instance=divisa, flag_choices=flag_choices)
         if form.is_valid():
             form.save()
     return redirect('divisa_list')
